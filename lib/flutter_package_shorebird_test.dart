cf580c4949012052bc1a854caa19c3519b8bec61
Omkar Parekh <omkarparekhneosoft@gmail.com>
2025-02-19 16:15:02 +0530

    Update Data

--- lib/flutter_package_shorebird_test.dart
+++ lib/flutter_package_shorebird_test.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
 class FlutterPackageShorebirdTest {
-  static MaterialColor fetchcolor() {
-    return Colors.blue;
+  static Color backgroundScafold() {
+    return Colors.white;
   }
+
+  static Color textColors() {
+    return Colors.black;
+  }
+
+  static List<Map<String, dynamic>> menuItems = [
+    {"icon": Icons.home, "title": "Home"},
+    {"icon": Icons.person, "title": "Profile"},
+    {"icon": Icons.settings, "title": "Settings"},
+    {"icon": Icons.logout, "title": "Logout"},
+  ];
 }
